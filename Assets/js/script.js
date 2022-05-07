ed2192a4b47b1cfa0cd6d571f53d2c670242c8f5
roxywasiak <ruksclone@hotmail.com>
2022-05-07 17:10:45 +0100

    started to create the elements fixed the on load to document ready and fixed the time with moment js different format

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,15 +1,11 @@
 //using moment.js to get the date format and i will add it to the #currentDay id
 const date = () => {
   //date format using moment js
-  const newDate = moment().format("dddd,  Do of  MMM, YYYY");
+  const newDate = moment().format("dddd, MMMM Do YYYY");
   $("#currentDay").append(newDate);
 };
 
-// WILL USE THIS IN THE RENDER TIMEBLOCKS
-// const currentHour = moment().hour;
-// console.log(currentHour);
-
-//the object array where the timeblocks labels are
+//the object array where the timeblocks labels are for each hour
 const workingDay = [
   { label: "9am", key: 9 },
   { label: "10am", key: 10 },
@@ -39,16 +35,43 @@ const clearLS = () => {
   localStorageGet.clear();
 };
 
+// WILL USE THIS IN THE RENDER TIMEBLOCKS
+// const currentHour = moment().hour;
+// console.log(currentHour);
 //show my timeblocks to appear on the page
 //create the elements and add them to the container
-//USE MOMENT.HOUR refer to the top where you wrote
-const renderTimeBlocks = (each) => {};
+const renderTimeBlocks = (each) => {
+  const timeNow = moment().hour;
+
+  const createElements = (each) => {
+    //gets all the timeblocks from html code adds to div
+    $("#container").append($("div"));
+    //adds the class for the timeblock second div
+    .addClass("time-block p-2")
+    sets the attributes 
+    .attr("data-key",  `${each.key}`)
+    .append (
+
+    )
+};
 
 //the save button to save in ls
-const saveBtn = () => {};
+const saveBtn = (event) => {
+  //the data within the key when the saveBtn is clicked
+  const keyClicked = $(event.target).attr("data-key");
+  //gets rid of things stored from savBtn from LS
+
+  //add the text items once you create with jquery
+  clearLS(keyClicked);
+  if (keyClicked) {
+    showPlanner();
+  }
+};
 
 //create the elements, set them give them attr
 const renderPlanner = () => {
+  //when clicked the events get handled
+  //?handle the savBtn click call it
   //show the blocks on the page
   renderTimeBlocks();
 };
@@ -61,4 +84,5 @@ const showPlanner = () => {
   renderPlanner();
 };
 
-$(window).on("load", showPlanner);
+//document.ready ? will check notes
+$(document).ready(showPlanner);
